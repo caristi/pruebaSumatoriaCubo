19d17ec87c78b1bc087b3268b9aec41645dec172
Carlos Aristizabal <carloshugo5@gmail.com>
2019-07-09 09:11:38 -0500

    Inicializar proyecto

---
+++ src/main/java/com/prueba/cubo/sumatoriaCubo/SumatoriaCuboApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.cubo.sumatoriaCubo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumatoriaCuboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumatoriaCuboApplication.class, args);
+	}
+
+}
